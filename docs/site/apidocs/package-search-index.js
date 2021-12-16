a880519f7e02810c2a7a0fb10e8674e5d1998b65
Eric Kolotyluk <eric@kolotyluk.net>
2021-12-16 12:55:56 -0800

    Repair missing javadoc

--- docs/site/apidocs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"net.kolotyluk.benchmark"},{"l":"net.kolotyluk.benchmark.jmh_generated"},{"l":"net.kolotyluk.loom"}];updateSearchResults();
